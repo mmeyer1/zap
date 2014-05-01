a318761f3bb69e69dd1d8b594e0878d1e1def24d
MEYER MATTHEW <Matt0719@gmail.com>
2014-05-01 09:41:21 -0400

    Initial Commit
    Naked app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zap::Application.config.secret_key_base = 'b64ab9ee2cd7dfebd0f35b06dc2765706cc186fcbea6a87009546eb2fac7853e711ae31b40ef453e5cbced789bae9d04ebd9bd1792e7e6f18bd3995ad25508fd'
