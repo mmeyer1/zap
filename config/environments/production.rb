21a48cbff818001181a4e604f988c0bcc0319145
MEYER MATTHEW <Matt0719@gmail.com>
2014-05-05 22:56:30 -0400

    Config settings for prod
    One-liner in the Prod env file to configure action mailer for
    production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Zap::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Configuring ActionMailer for Devise 
+  config.action_mailer.default_url_options = { :host => 'obscure-mesa-1086.heroku.com'}
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
